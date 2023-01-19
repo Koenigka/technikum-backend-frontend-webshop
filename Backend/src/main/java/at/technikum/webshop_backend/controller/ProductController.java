2a6626cb76b25b9c0fc43af8cdef8f3e674826d6
Karin König <k.koenig@outlook.at>
2023-01-19 13:02:19 +0100

    bootstrap_template

--- Backend/src/main/java/at/technikum/webshop_backend/controller/ProductController.java
+++ Backend/src/main/java/at/technikum/webshop_backend/controller/ProductController.java
@@ -1,4 +1,61 @@
 package at.technikum.webshop_backend.controller;
 
+import at.technikum.webshop_backend.model.Product;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
 public class ProductController {
+
+    private List<Product> products;
+
+    public ProductController(){
+
+        this.products = new ArrayList<>();
+        this.products.add(new Product(1L, "Chocolate Cookie", "wonderfull Cookie, with dark chocolate",1L, 5.99, 100));
+        this.products.add(new Product(2L, "White Cookie", "wonderfull Cookie, with white chocolate",2L, 6.99, 150));
+
+    }
+
+    @PostMapping("")
+    public Product create(Product product){
+
+        this.products.add(product);
+        return product;
+    }
+
+    @GetMapping("")
+    public List<Product> readAll(){
+
+        return this.products;
+    }
+
+
+    @GetMapping ("/{id}")
+    public Product read(@PathVariable Long id){
+
+        for (Product product : this.products) {
+            if(id.equals( product.getId())){
+                return product;
+            }
+        }
+        //404 question not found
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+    }
+
+    @PutMapping("/{id}")
+    public Product update(){
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    public Product delete(){
+        return null;
+    }
 }
+
--- Backend/src/main/java/at/technikum/webshop_backend/model/Product.java
+++ Backend/src/main/java/at/technikum/webshop_backend/model/Product.java
@@ -1,4 +1,76 @@
 package at.technikum.webshop_backend.model;
 
 public class Product {
+
+    private Long id;
+
+    private String title;
+
+    private String description;
+
+    private long fid_category;
+
+    private double price;
+
+    private int stock;
+
+    public Product(Long id, String title, String description, long fid_category, double price, int stock) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.fid_category = fid_category;
+        this.price = price;
+        this.stock = stock;
+    }
+
+    public Product() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public long getFid_category() {
+        return fid_category;
+    }
+
+    public void setFid_category(long fid_category) {
+        this.fid_category = fid_category;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
 }
