95e8280e9069851900e791ccfd12f96a85c59f80
Spyridoula <spyrarvaniti@gmail.com>
2023-02-07 15:34:56 +0100

    UserService & UserController

--- Backend/src/main/java/at/technikum/webshop_backend/controller/UserController.java
+++ Backend/src/main/java/at/technikum/webshop_backend/controller/UserController.java
@@ -1,4 +1,30 @@
 package at.technikum.webshop_backend.controller;
 
+import at.technikum.webshop_backend.model.User;
+import at.technikum.webshop_backend.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public User createUser(@RequestBody User user){
+        return userService.save(user);
+    }
+
+    @GetMapping("/{id}")
+    public User findById(@PathVariable Long id){
+        return userService.findById(id);
+    }
+
 }
