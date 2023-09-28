78a7d383cd178171c18bdae73005c2672779ba58
Spyridoula <spyrarvaniti@gmail.com>
2023-09-28 15:38:10 +0200

    added @ActiveProfiles("test") in CartItemControllerTest

--- Backend/src/test/java/at/technikum/webshop_backend/controllerTest/CartItemControllerTest.java
+++ Backend/src/test/java/at/technikum/webshop_backend/controllerTest/CartItemControllerTest.java
@@ -16,6 +16,7 @@ import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -25,6 +26,7 @@ import static org.mockito.Mockito.when;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 public class CartItemControllerTest {
 
     @Autowired
