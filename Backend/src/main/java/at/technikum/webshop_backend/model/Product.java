022bcac0e0c0ddf765b18330c459afe79a897164
Karin König <k.koenig@outlook.at>
2023-10-08 16:57:59 +0200

    added logic controller for valid, correct s issues

--- Backend/src/main/java/at/technikum/webshop_backend/model/Product.java
+++ Backend/src/main/java/at/technikum/webshop_backend/model/Product.java
@@ -4,10 +4,12 @@ import at.technikum.webshop_backend.dto.ProductDto;
 import at.technikum.webshop_backend.utils.ConvertableToDto;
 import jakarta.persistence.*;
 
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -24,18 +26,28 @@ public class Product implements ConvertableToDto<ProductDto>, Cloneable{
     @Column(name = "id")
     private Long id;
 
+    @NotBlank
+    @Size(max = 255)
     private String title;
 
+    @NotBlank
+    @Length(max = 1000)
     private String description;
 
+    @NotBlank
+    @Length(max = 1000)
     private String img;
 
+    @DecimalMin(value = "0.01")
     private double price;
 
+    @PositiveOrZero
     private int stock;
 
+    @NotNull
     private Boolean active;
 
+    @NotNull
     @ManyToOne(optional = false)
     @JoinColumn(name = "category_id", nullable = false)
     private Category category;
