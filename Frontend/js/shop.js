19f04534df436d0852aa7887d7b3cec74f1e4fdd
Spyridoula <spyrarvaniti@gmail.com>
2023-10-23 12:59:33 +0200

    footer fixed-bottom

--- Frontend/js/shop.js
+++ Frontend/js/shop.js
@@ -215,9 +215,9 @@ $(document).ready(function () {
       success: function (products) {
         addProducts(products);
         // Check if there are no products for this category
-        if (products.length === 0) {
+        /*  if (products.length === 0) {
           $(".footer").addClass("fixed-bottom");
-        }
+        } */
       },
       error: function (error) {
         console.error(error);
